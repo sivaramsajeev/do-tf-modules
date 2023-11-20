65dde5ccf191d09e485ae785df7d660244f3af7f
sivaram sajeev <sivaramsajeev@sivarams-MacBook-Air.local>
2023-11-20 13:39:29 +0530

    db exports

--- db/outputs.tf
+++ db/outputs.tf
@@ -1,3 +1,23 @@
 output "db_private_link" {
   value = digitalocean_database_cluster.do-db.private_host
 }
+
+output "db_id" {
+  value = digitalocean_database_cluster.do-db.id
+}
+
+output "db_host" {
+  value = digitalocean_database_cluster.do-db.host
+}
+
+output "db_port" {
+  value = digitalocean_database_cluster.do-db.port
+}
+
+output "db_username" {
+  value = digitalocean_database_cluster.do-db.user
+}
+
+output "db_password" {
+  value = digitalocean_database_cluster.do-db.password
+}
